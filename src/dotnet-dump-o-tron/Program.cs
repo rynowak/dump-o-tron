542682a950e80f9ac0e733a6974f253c0fd21c6c
Ryan Nowak <rynowak@microsoft.com>
2020-01-20 23:24:21 -0800

    Initial skeleton

---
+++ src/dotnet-dump-o-tron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_dump_o_tron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
